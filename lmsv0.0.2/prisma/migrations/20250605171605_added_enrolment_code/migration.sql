20117b10c85ddbfbd9765ce53271fedba953ead5
BenJr23 <rubenjrtbertuso@gmail.com>
2025-06-14 15:02:11 +0800

    g luck

--- lmsv0.0.2/prisma/migrations/20250605171605_added_enrolment_code/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `enrolmentCode` to the `SubjectInstance` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "SubjectInstance" ADD COLUMN     "enrolmentCode" INTEGER NOT NULL;
